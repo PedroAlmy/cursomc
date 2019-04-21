450d029271723a0756673754cc2aeeeda5b98bed
Pedro Almeida <pedrogunsx@gmail.com>
2019-04-20 21:31:01 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,31 @@
 package com.udemy.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping (value= "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar () {
+	public List<Categoria> listar () {
 		
-		return "REST está funcionando";
+		Categoria categoriaInformatica = new Categoria (1, "Informatica");
+		Categoria categoriaEscritorio = new Categoria (2, "Escritorio");
+		
+		List <Categoria> listaDeCategoria = new ArrayList <>();
+		
+		listaDeCategoria.add(categoriaInformatica);
+		listaDeCategoria.add(categoriaEscritorio);
+		
+		
+		return listaDeCategoria;
 		
 	}
 	
